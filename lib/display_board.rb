2919cff333ffca5059e1ebd0e63f96f059e9b690
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-21 14:10:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
+def display_board(board)
 
 "   |   |   "
 "-----------"
 "   |   |   "
 "-----------"
 "   |   |   "
+end 
